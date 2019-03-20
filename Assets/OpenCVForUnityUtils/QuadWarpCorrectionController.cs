252bd97912a94052d0f1ddcb19f53b9f29ac1905
shimizuyudai <shimizu.yudai.0730@gmail.com>
2019-03-21 06:43:07 +0900

    update

---
+++ Assets/OpenCVForUnityUtils/QuadWarpCorrectionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadWarpCorrectionController : TextureHolderBase
+{
+    [SerializeField]
+    Camera camera;
+    RenderTexture renderTexture;
+    [SerializeField]
+    Vector2 aspect;
+
+    public override Texture GetTexture()
+    {
+        return renderTexture;
+    }
+
+
+
+
+}
